1a2521ad9e02439033e0c99c3c1ad2d08f58657e
dulz01 <yuureijin@gmail.com>
2016-11-25 21:19:11 +0000

    Added a test for rules system

--- L-Systems/src/examples/example_lsystems/example_lsystems.h
+++ L-Systems/src/examples/example_lsystems/example_lsystems.h
@@ -9,7 +9,7 @@ namespace octet {
   class example_lsystems : public app {
     // scene for drawing box
     ref<visual_scene> app_scene;
-    
+
     // L-systems variables
     std::string axiom;
 
@@ -20,21 +20,38 @@ namespace octet {
 
     std::vector<rule> rules;
 
+    int counter;
 
   public:
     /// this is called when we construct the class before everything is initialised.
     example_lsystems(int argc, char **argv) : app(argc, argv) {
-      axiom = "A";
+      axiom = "F";
       rules = {
-        {'A', "ABA"},
-        {'B', "A"}
+        {'F', "F[+F]F[-F]F"}
       };
+      counter = 0;
+
+    }
+
+    void applyRules() {
+      std::string new_str;
+      new_str = axiom;
+      for (int i = 0; i < (int)axiom.length(); i++) {
+        for (int j = 0; i < (int)rules.size(); j++) {
+          if (axiom.at(i) == rules.at(j).variable) {
+            new_str.erase(i);
+            new_str.append(rules.at(j).replacement);
+            axiom = new_str;
+            break;
+          }
+        }
+      }
 
     }
 
     /// this is called once OpenGL is initialized
     void app_init() {
-      app_scene =  new visual_scene();
+      app_scene = new visual_scene();
       app_scene->create_default_camera_and_lights();
 
       material *red = new material(vec4(1, 0, 0, 1));
@@ -50,8 +67,13 @@ namespace octet {
       get_viewport_size(vx, vy);
       app_scene->begin_render(vx, vy);
 
+      if (counter < 5) {
+        std::cout << "Iteration " << counter++ << " : "<< axiom << std::endl;
+        applyRules();
+      }
+
       // update matrices. assume 30 fps.
-      app_scene->update(1.0f/30);
+      app_scene->update(1.0f / 30);
 
       // draw the scene
       app_scene->render((float)vx / vy);
