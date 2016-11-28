b0ba476cb515a4b37a753f5f079f89c1b89751e1
dulz01 <yuureijin@gmail.com>
2016-11-28 18:22:06 +0000

    The trees now rescale to the screen coordinates instead of their own.

--- L-Systems/src/examples/example_lsystems/turtle.h
+++ L-Systems/src/examples/example_lsystems/turtle.h
@@ -12,15 +12,15 @@ class turtle {
     point end;
   };
 
+  point max_position_;
+  point min_position_;
   point position_;
 
   line path_;
 
   float pi_;
   float distance_;
-  bool out_of_view;
 
-  std::vector<point> all_points_;
   std::vector<point> branch_points_;
   std::vector<line> lines_;
 
@@ -33,10 +33,25 @@ private:
     path_.start = position_;
     position_.x = next_x;
     position_.y = next_y;
-
     path_.end = position_;
+    
 
-    all_points_.push_back(position_);
+    if (min_position_.x > position_.x) {
+      min_position_.x = position_.x;
+    }
+
+    if (min_position_.y > position_.y) {
+      min_position_.y = position_.y;
+    }
+    if (max_position_.x < position_.x) {
+      max_position_.x = position_.x;
+    }
+
+    if (max_position_.y < position_.y) {
+      max_position_.y = position_.y;
+    }
+
+
     lines_.push_back(path_);
   }
 
@@ -58,31 +73,31 @@ private:
   }
 
   void reset() {
-    position_.x = 0.0f;
-    position_.y = -1.0f;
-    position_.dir = 90.0f;
+    
+    position_.x = max_position_.x = min_position_.x = 0.0f;
+    position_.y = max_position_.y = min_position_.y = -1.0f;
+    position_.dir = max_position_.dir = min_position_.dir = 90.0f;
   }
 
-  bool is_in_view(point p) {
-    return (p.x >= -1 && p.x <= 1 && p.y >= -1 && p.y <= 1);
-  }
+  // converting the range from the tree to the range of the screen.
+  // so we can scale the tree to fit into the camera view.
+  // formula taken from Greg Neill's post 
+  // on: https://groups.google.com/forum/#!topic/alt.math/sj4tTuXpxE0
+  void rescale() {
+    float tree_range_x = max_position_.x - min_position_.x;
+    float tree_range_y = max_position_.y - min_position_.y;
+    float screen_range = 1 - (-1);
 
-  bool check_within_bounds() {
-    for (std::vector<point>::iterator it = all_points_.begin();
-      it != all_points_.end(); ++it) {
-      if (!is_in_view(*it)) {
-        return false;
-      } // end of if statement
-      else {
-        return true;
-      } // end of else statement
-    } // end of for loop
+    for (int i = 0; i < lines_.size(); i++) {
+      lines_[i].start.x = (max_position_.x * -1 - min_position_.x * 1) / (tree_range_x) + lines_[i].start.x * (screen_range) / (tree_range_x);
+      lines_[i].end.x = (max_position_.x * -1 - min_position_.x * 1) / (tree_range_x)+lines_[i].end.x * (screen_range) / (tree_range_x);
+      lines_[i].start.y = (max_position_.y * -1 - min_position_.y * 1) / (tree_range_y)+lines_[i].start.y * (screen_range) / (tree_range_y);
+      lines_[i].end.y = (max_position_.y * -1 - min_position_.y * 1) / (tree_range_y)+lines_[i].end.y * (screen_range) / (tree_range_y);
+    }
   }
 
 public:
-  turtle() : pi_(3.141592653f), distance_(0.125f) {
-    out_of_view = true;
-
+  turtle() : pi_(3.141592653f), distance_(1.0f) {
     reset();
   }
 
@@ -99,26 +114,28 @@ public:
   }
 
   void generate_tree(std::string& treeStringMap, float rotationAngle) {
-      lines_.clear();
-      branch_points_.clear();
-      for (int i = 0; i < treeStringMap.length(); i++) {
-        if (treeStringMap.at(i) == 'F') {
-          go_forward();
-        }
-        else if (treeStringMap.at(i) == '+') {
-          turn_left(rotationAngle);
-        }
-        else if (treeStringMap.at(i) == '-') {
-          turn_right(rotationAngle);
-        }
-        else if (treeStringMap.at(i) == '[') {
-          new_branch();
-        }
-        else if (treeStringMap.at(i) == ']') {
-          return_to_trunk();
-        }
-      } // end of for loop
+    lines_.clear();
+    branch_points_.clear();
+    for (int i = 0; i < treeStringMap.length(); i++) {
+      if (treeStringMap.at(i) == 'F') {
+        go_forward();
+      }
+      else if (treeStringMap.at(i) == '+') {
+        turn_left(rotationAngle);
+      }
+      else if (treeStringMap.at(i) == '-') {
+        turn_right(rotationAngle);
+      }
+      else if (treeStringMap.at(i) == '[') {
+        new_branch();
+      }
+      else if (treeStringMap.at(i) == ']') {
+        return_to_trunk();
+      }
+    } // end of for loop
 
-      reset();
+    rescale();
+
+    reset();
   }
 };
